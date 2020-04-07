7c1cfb9ea36e30432c14d9723564d5d1ea6dcfc6
atobaum <atobaum@gmail.com>
2020-04-07 23:09:04 +0900

    fail: 1916 due to time over

---
+++ 1916.py
@@ -0,0 +1,26 @@
+from queue import PriorityQueue
+
+def parse():
+    return list(map(int, input().split()))
+
+def dji(adj, s, e):
+    q = PriorityQueue()
+    q.put((0, s))
+
+    while not q.empty():
+        cost, here = q.get()
+        if here == e:
+            return cost
+        for there, deltaCost in adj[here]:
+            q.put((cost+deltaCost, there))
+
+n, = parse()
+m, = parse()
+
+adj = [[] for _ in range(m)]
+for _ in range(m):
+    a, b, c = parse()
+    adj[a].append((b, c))
+
+s, e = parse()
+print(dji(adj, s, e))
