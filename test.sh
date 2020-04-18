531b7d025d158995450fd5c2b9b904663b971dee
atobaum <atobaum@gmail.com>
2020-04-18 21:26:02 +0900

    Add test script

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]; then
+	echo "Usage: $0 (problem number)"
+fi
+
+test_files=`ls | grep "^$1[.-]"`
+i=0
+for filename in $test_files; do
+	i=i+1
+	ret=`python3 $1.py < $filename`
+	echo "Test $filename:"
+	echo "$ret"
+done
