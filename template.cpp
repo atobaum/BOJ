cec606fff938e73930bff1464d5832b322509a15
atobaum <atobaum@gmail.com>
2020-11-24 00:01:20 +0900

    C++ 템플릿을 만들자

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <algorithm> //sort
+#include <cstring>
+#include <functional> //greater
+#include <iostream>
+#include <queue>
+#include <utility> //pair
+#include <vector>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  return 0;
+}
