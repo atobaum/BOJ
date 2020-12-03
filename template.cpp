ce90ac9bd663c8ede2b6d23c5309c9b5ef0fd514
atobaum <atobaum@gmail.com>
2020-12-03 17:15:35 +0900

    템플릿을 업데이트하자
    io용 매크로 추가,
    climits 헤더 추가

--- template.cpp
+++ template.cpp
@@ -1,4 +1,5 @@
-#include <algorithm>  //sort
+#include <algorithm> //sort
+#include <climits>
 #include <cstring>    //memset
 #include <functional> //greater
 #include <iostream>
@@ -6,6 +7,19 @@
 #include <utility> //pair
 #include <vector>
 using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+#define reap(i, a, b) for (int i = a; i < b; i++)
+#define in1(a) cin >> a;
+#define in2(a, b) cin >> a >> b;
+#define in3(a, b, c) cin >> a >> b >> c;
+#define in4(a, b, c, d) cin >> a >> b >> c >> d;
+#define out1(a) cout << a << endl;
+#define out2(a, b) cout << a << " " << b << endl;
+#define out3(a, b, c) cout << a << " " << b << " " << c << endl;
+#define out4(a, b, c, d) cout << a << " " << b << " " << c << " " << d << endl;
 
 int main() {
   ios::sync_with_stdio(false);
