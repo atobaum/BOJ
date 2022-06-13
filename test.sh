02400503e36dc2ed3801821130d4ccd4c187059d
atobaum <atobaum@gmail.com>
2022-06-13 23:15:59 +0900

    write a simple test runner

--- test.sh
+++ test.sh
@@ -1,11 +1,3 @@
-FILENAME="test.txt"
-if [ -n $1 ]; then
-	FILENAME=$1
-fi
-RESULT=`awk 'BEGIN {RS=""} NR==1{print $0}' "$FILENAME" | ./a.out`
-ANSWER=`awk 'BEGIN {RS=""} NR==2{print $0}' "$FILENAME"`
-echo "result:"
-echo "$RESULT"
-echo ""
-echo "answer:"
-echo "$ANSWER"
+TEST=`cat "$1.in" | ./a.out`
+
+echo $TEST | diff "$1.out" -
